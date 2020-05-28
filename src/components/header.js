2efc6a0c469a2c8e14941a97be20f4d9f7397a86
Beamer <beamercola@gmail.com>
2020-05-27 23:08:35 -0700

    Logo

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,14 @@
 import { Link } from "gatsby"
 import React from "react"
+import Logo from "../images/logo.svg"
 
 const Header = () => (
   <header className="fixed top-0 inset-x-0 xl:px-24">
-    <div className="container border-b-2 border-black">
+    <div className="container border-b-2 border-black py-4">
       <h1 className="text-center text-6xl">
-        <Link to="/">Defender</Link>
+        <Link to="/">
+          <img className="h-32 mx-auto" src={Logo} />
+        </Link>
       </h1>
     </div>
   </header>
