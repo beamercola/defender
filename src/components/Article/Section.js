106efe9b5bc675d818e044daf5c66d80378b3a9d
Beamer <beamer@hey.com>
2021-01-24 20:43:37 -0800

    Sticky callout

--- src/components/Article/Section.js
+++ src/components/Article/Section.js
@@ -4,7 +4,9 @@ import { Fade } from "react-awesome-reveal"
 const Section = ({ callout, children }) => (
   <div className="md:flex md:items-stretch">
     <div className="md:w-80 flex-shrink-0">
-      <Fade triggerOnce>{callout}</Fade>
+      <Fade className="sticky top-24" triggerOnce>
+        {callout}
+      </Fade>
     </div>
     <div className="flex-grow border-l px-5 md:px-16">{children}</div>
   </div>
