2f6c7bba17b6bde94ae9df73c3829c5d9dc6cb15
Beamer <beamer@hey.com>
2021-01-31 22:55:07 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,16 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => {
-  return (
-    <Layout>
-      <SEO title="Home" />
-      <div className="fixed inset-0 flex flex-col items-center">
-        <img src="/defender-d.svg" className="" style={{ height: "80%" }} />
-        <h1 className="font-bureau mt-12 text-2xl uppercase flex-shrink-0">
-          Coming Soon
-        </h1>
-      </div>
-    </Layout>
-  )
-}
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <div className="fixed inset-0 flex flex-col items-center">
+      <img src="/defender-d.svg" className="" style={{ height: "80%" }} />
+      <h1 className="font-bureau mt-12 text-2xl uppercase flex-shrink-0">
+        Coming Soon
+      </h1>
+    </div>
+  </Layout>
+)
 
 export default IndexPage
