0eb60face4b2890d67a90e4a565afb573d5ae9f6
Beamer <beamer@hey.com>
2021-02-02 23:01:42 -0800

    Gif

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,20 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div className="fixed inset-0 flex flex-col items-center">
-      <img src="/defender-d.svg" className="" style={{ height: "80%" }} />
-      <h1 className="font-bureau mt-12 text-2xl uppercase flex-shrink-0">
+      <img className="h-24 mb-8" src="/logo.svg" alt="Defender" />
+      <img
+        src="/defender_gif.gif"
+        className="mb-12"
+        style={{ height: "50%" }}
+        alt=""
+      />
+      <p className="mb-4 max-w-sm text-center">
+        An art and media project about the work of the{" "}
+        <span class="whitespace-no-wrap">
+          San Francisco Public Defender’s Office
+        </span>
+      </p>
+      <h1 className="font-bureau text-2xl uppercase flex-shrink-0">
         Coming Soon
       </h1>
       <a
