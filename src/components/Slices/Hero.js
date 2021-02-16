90ba3315df5412c6ba6c2aca56ff197a94858ea9
Beamer <beamer@hey.com>
2021-02-15 16:27:18 -0800

    New cover

--- src/components/Slices/Hero.js
+++ src/components/Slices/Hero.js
@@ -6,39 +6,50 @@ import { Fade } from "react-awesome-reveal"
 import Cover from "../Cover"
 import Link from "../Link"
 
-const Hero = ({ slice: { primary: cover } }) => (
-  <Tippy
-    render={attrs => (
-      <div
-        className="bg-yellow p-2 w-72 font-mono text-sm"
-        dangerouslySetInnerHTML={{ __html: cover?.cursor_text?.html }}
-        {...attrs}
-      />
-    )}
-    plugins={[followCursor]}
-    followCursor={true}
-    placement="bottom-start"
-    disabled={!cover?.cursor_text?.html?.length > 0}
-  >
-    <div className="">
-      <Fade>
-        <Link to={cover.link?.uid && `/${cover.link?.uid}`}>
-          <Cover
-            className="h-80vh border-b border-t"
-            image={cover.image.url}
-            video={cover.video?.url}
-          >
-            <Fade>
-              <h1 className="font-black font-bureau-wide text-yellow uppercase text-4xl lg:text-5xl text-center">
-                {cover?.content?.text}
-              </h1>
-            </Fade>
-          </Cover>
-        </Link>
-      </Fade>
-    </div>
-  </Tippy>
-)
+const Hero = ({ slice: { primary: cover } }) => {
+  console.log(cover)
+  return (
+    <Tippy
+      render={attrs => (
+        <div
+          className="bg-yellow p-2 w-72 font-mono text-sm"
+          dangerouslySetInnerHTML={{ __html: cover?.cursor_text?.html }}
+          {...attrs}
+        />
+      )}
+      plugins={[followCursor]}
+      followCursor={true}
+      placement="bottom-start"
+      disabled={!cover?.cursor_text?.html?.length > 0}
+    >
+      <div className="">
+        <Fade>
+          <Link to={cover.link?.uid && `/${cover.link?.uid}`}>
+            <Cover
+              className="h-80vh border-b border-t"
+              image={cover.image.url}
+              video={cover.video?.url}
+            >
+              {/* <Fade> */}
+              {cover?.content?.text && (
+                <h1 className="font-black font-bureau-wide text-yellow uppercase text-4xl lg:text-5xl text-center">
+                  {cover?.content?.text}
+                </h1>
+              )}
+              {cover?.overlay && (
+                <div
+                  className="absolute inset-0 bg-contain bg-center bg-no-repeat"
+                  style={{ backgroundImage: `url(${cover?.overlay?.url})` }}
+                />
+              )}
+              {/* </Fade> */}
+            </Cover>
+          </Link>
+        </Fade>
+      </div>
+    </Tippy>
+  )
+}
 
 export default Hero
 
@@ -49,6 +60,9 @@ export const fragments = graphql`
     primary {
       image {
         alt
+        url(imgixParams: { w: 1200 })
+      }
+      overlay {
         url(imgixParams: { w: 1200 })
       }
       content: text {
